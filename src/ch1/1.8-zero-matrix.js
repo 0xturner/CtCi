279dd5f1c8238a38f22d26d4466636b01aff0890
Turner <turnerschumann@gmail.com>
2022-11-24 13:22:35 -0800

    optimize q 1.8

--- src/ch1/1.8-zero-matrix.js
+++ src/ch1/1.8-zero-matrix.js
@@ -1,6 +1,6 @@
 /**
- * time: 0(nm)
- * space: (nm)
+ * time: O(nm)
+ * space: O(nm)
  */
 const zeroMatrix = (matrix) => {
   const N = matrix.length; // height
@@ -31,6 +31,77 @@ const zeroMatrix = (matrix) => {
   return matrix;
 };
 
+/**
+ * a more space efficient version of the same function
+ *
+ * time: O(nm)
+ * space: O(1)
+ */
+const optimizedZeroMatrix = (matrix) => {
+  const N = matrix.length; // height
+  const M = matrix[0].length; // width
+  let firstRowZero = false;
+  let firstColZero = false;
+
+  for (let row = 0; row < N; row++) {
+    if (matrix[row][0] === 0) {
+      firstColZero = true;
+      break;
+    }
+  }
+
+  for (let col = 0; col < M; col++) {
+    if (matrix[0][col] === 0) {
+      firstRowZero = true;
+      break;
+    }
+  }
+
+  // check and record zeroes
+  for (let row = 0; row < N; row++) {
+    for (let col = 0; col < M; col++) {
+      if (matrix[row][col] === 0) {
+        matrix[0][col] = 0;
+        matrix[row][0] = 0;
+      }
+    }
+  }
+
+  const nullifyRow = (rowIdx) => {
+    for (let col = 0; col < M; col++) {
+      matrix[rowIdx][col] = 0;
+    }
+  };
+
+  const nullifyCol = (colIdx) => {
+    for (let row = 0; row < N; row++) {
+      matrix[row][colIdx] = 0;
+    }
+  };
+
+  for (let row = 1; row < N; row++) {
+    if (matrix[row][0] === 0) {
+      nullifyRow(row);
+    }
+  }
+
+  for (let col = 1; col < M; col++) {
+    if (matrix[0][col] === 0) {
+      nullifyCol(col);
+    }
+  }
+
+  if (firstRowZero) {
+    nullifyRow(0);
+  }
+
+  if (firstColZero) {
+    nullifyColumn(0);
+  }
+
+  return matrix;
+};
+
 const test = [
   [1, 2, 3],
   [4, 5, 6],
@@ -38,4 +109,13 @@ const test = [
   [10, 11, 12],
 ];
 
-console.log(zeroMatrix(test));
+const test2 = [
+  [1, 2, 0],
+  [4, 5, 6],
+  [7, 8, 9],
+  [10, 11, 12],
+];
+
+console.log(zeroMatrix(structuredClone(test)));
+console.log(optimizedZeroMatrix(structuredClone(test)));
+console.log(optimizedZeroMatrix(structuredClone(test2)));
