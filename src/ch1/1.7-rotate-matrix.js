3ebfbb5d75b8907d4a9ebd0eaf9ab02ffe105d4c
Turner <turnerschumann@gmail.com>
2022-11-24 11:51:59 -0800

    q 1.7

--- src/ch1/1.7-rotate-matrix.js
+++ src/ch1/1.7-rotate-matrix.js
@@ -1,17 +1,25 @@
-/*
- * To open a new Quokka file:
- *   - Press `⌘ K, J` to create a new JavaScript File restart Quokka on an existing file:
- *   - Press `⌘ K, Q`
+/**
+ * time: O(n^2)
+ * space: O(1)
  */
-
 const rotateMatrix = (matrix) => {
   const N = matrix.length;
 
+  // transpose i.e (x,y) -> (y,x)
   for (let row = 0; row < N; row++) {
     for (let col = row; col < N; col++) {
       const temp = matrix[row][col];
       matrix[row][col] = matrix[col][row];
       matrix[col][row] = temp;
+    }
+  }
+
+  // flip horizontaly
+  for (let row = 0; row < N; row++) {
+    for (let col = 0; col < N / 2; col++) {
+      const temp = matrix[row][col];
+      matrix[row][col] = matrix[row][N - 1 - col];
+      matrix[row][N - 1 - col] = temp;
     }
   }
 
