158f9903963c076c7fdc77f38aa558b9cb33b516
Turner <turnerschumann@gmail.com>
2022-11-24 17:28:30 -0800

    q 2.1 refactor

--- src/ch2/2.1-remove-dups.js
+++ src/ch2/2.1-remove-dups.js
@@ -1,27 +1,24 @@
 /**
+ * assumes singly linked list is passed
+ *
  * time: O(n)
  * space: O(n)
  */
 const removeDups = (head) => {
   const set = new Set(head.value);
-  set.add(head.data);
 
-  const removeNextNodeIfDup = (node) => {
-    if (!node?.next) {
-      return;
-    }
+  let curr = head;
+  let previous = null;
 
-    if (set.has(node.next.data)) {
-      node.next = node.next.next;
-      removeNextNodeIfDup(node);
+  while (curr) {
+    if (set.has(curr.data)) {
+      previous.next = curr.next;
     } else {
-      set.add(node.next.data);
+      set.add(curr.data);
+      previous = curr;
     }
-
-    removeNextNodeIfDup(node.next);
-  };
-
-  removeNextNodeIfDup(head);
+    curr = curr.next;
+  }
 
   return head;
 };
@@ -31,6 +28,7 @@ const removeDups = (head) => {
  *
  * time: O(n^2)
  * space: O(1)
+ *
  */
 const removeDupsSpaceEfficient = (head) => {
   let curr = head;
@@ -49,10 +47,37 @@ const removeDupsSpaceEfficient = (head) => {
   return head;
 };
 
+/**
+ * assumes doubly linked list is passed
+ *
+ * time: O(n)
+ * space: O(n)
+ */
+const removeDupsDoublyLinked = (head) => {
+  const set = new Set(head.value);
+
+  let curr = head;
+
+  while (curr) {
+    if (set.has(curr.data)) {
+      curr.prev.next = curr.next;
+      if (curr.next) {
+        curr.next.prev = curr.prev;
+      }
+    } else {
+      set.add(curr.data);
+    }
+    curr = curr.next;
+  }
+
+  return head;
+};
+
 class Node {
   constructor(data) {
     this.data = data;
     this.next = null;
+    this.prev = null;
   }
 }
 
@@ -61,10 +86,16 @@ const two = new Node("b");
 const three = new Node("c");
 const four = new Node("b");
 const five = new Node("a");
+
 one.next = two;
 two.next = three;
+two.prev = one;
 three.next = four;
+three.prev = two;
 four.next = five;
+four.prev = three;
+five.prev = four;
 
 console.log(removeDups(structuredClone(one)));
 console.log(removeDupsSpaceEfficient(structuredClone(one)));
+console.log(removeDupsDoublyLinked(structuredClone(one)));
